79e31f0714becf5963c09e774d0db29571c8c16f
Vlad Flore <flore.vlad@gmail.com>
2021-02-08 12:08:41 +0100

    Refactor integration test

--- src/test/java/com/example/demo/DemoApplicationIT.java
+++ src/test/java/com/example/demo/DemoApplicationIT.java
@@ -32,49 +32,59 @@ public class DemoApplicationIT {
 	private TestRestTemplate restTemplate;
 
 	@Test
-	@DisplayName("when two valid instrument ticks are POSTed, then the instrument stats are calculated correctly")
-	void test() throws URISyntaxException {
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-
-		String instrumentId = "IBM.N";
-
-		InstrumentTick instrumentTick = new InstrumentTick(instrumentId, 100, new Timestamp(System.currentTimeMillis()));
-		HttpEntity<InstrumentTick> httpEntity = new HttpEntity<>(instrumentTick, headers);
-
-		URI uri = new URI("http://localhost:" + port + "/ticks");
-
-		ResponseEntity<Void> response = restTemplate.postForEntity(uri, httpEntity, Void.class);
+	@DisplayName("when two valid instrument ticks are POSTed (successively), then the instrument stats are fetched (GET) and calculated correctly")
+	void whenValidTicksAreCreatedForAnInstrument_thenStatsAreCorrectlyFetchedAndComputed() throws URISyntaxException {
+		HttpHeaders headers = createHeaders();
+		InstrumentTick instrumentTick = createInstrumentTick("IBM.N", 100, System.currentTimeMillis());
+		HttpEntity<InstrumentTick> httpEntity = createHttpEntity(headers, instrumentTick);
+		URI uriPost = createUri("http", "localhost", port, "ticks");
+		ResponseEntity<Void> response = restTemplate.postForEntity(uriPost, httpEntity, Void.class);
 
 		URI location = response.getHeaders().getLocation();
 		assertThat(location).isNotNull();
 		assertThat(location.toString()).contains("/statistics/" + instrumentTick.getInstrument());
 		assertThat(response.getStatusCodeValue()).isEqualTo(HttpStatus.CREATED.value());
 
-		Statistics statistics = restTemplate.getForObject(URI.create("http://localhost:" + port + "/statistics/" + instrumentId), Statistics.class);
-
+		URI uriGet = createUri("http", "localhost", port, "statistics/" + instrumentTick.getInstrument());
+		Statistics statistics = restTemplate.getForObject(uriGet, Statistics.class);
 		assertThat(statistics).isNotNull();
 		assertThat(statistics.getAvg()).isEqualTo(instrumentTick.getPrice());
 		assertThat(statistics.getCount()).isEqualTo(1);
 		assertThat(statistics.getMin()).isEqualTo(instrumentTick.getPrice());
 		assertThat(statistics.getMax()).isEqualTo(instrumentTick.getPrice());
 
-		instrumentTick = new InstrumentTick(instrumentId, 200, new Timestamp(System.currentTimeMillis()));
-		httpEntity = new HttpEntity<>(instrumentTick, headers);
-
-		response = restTemplate.postForEntity(uri, httpEntity, Void.class);
+		instrumentTick = createInstrumentTick("IBM.N", 200, System.currentTimeMillis());
+		httpEntity = createHttpEntity(headers, instrumentTick);
+		response = restTemplate.postForEntity(uriPost, httpEntity, Void.class);
 
 		location = response.getHeaders().getLocation();
 		assertThat(location).isNotNull();
 		assertThat(location.toString()).contains("/statistics/" + instrumentTick.getInstrument());
 		assertThat(response.getStatusCodeValue()).isEqualTo(HttpStatus.CREATED.value());
 
-		statistics = restTemplate.getForObject(URI.create("http://localhost:" + port + "/statistics/" + instrumentId), Statistics.class);
-
+		statistics = restTemplate.getForObject(uriGet, Statistics.class);
 		assertThat(statistics).isNotNull();
 		assertThat(statistics.getAvg()).isEqualTo(150);
 		assertThat(statistics.getCount()).isEqualTo(2);
 		assertThat(statistics.getMin()).isEqualTo(100);
 		assertThat(statistics.getMax()).isEqualTo(200);
+	}
+
+	private URI createUri(String protocol, String host, int port, String endpoint) throws URISyntaxException {
+		return new URI(protocol + "://" + host + ":" + port + "/" + endpoint);
+	}
+
+	private InstrumentTick createInstrumentTick(String instrument, double price, long millis) {
+		return new InstrumentTick(instrument, price, new Timestamp(millis));
+	}
+
+	private HttpEntity<InstrumentTick> createHttpEntity(HttpHeaders headers, InstrumentTick instrumentTick) {
+		return new HttpEntity<>(instrumentTick, headers);
+	}
+
+	private HttpHeaders createHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		return headers;
 	}
 }
